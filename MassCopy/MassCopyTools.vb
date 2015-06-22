dc2e9b9a0e7b8d8ff9c650b57edf033dae04d45c
Ian <ilantzy@udel.edu>
2015-06-22 14:45:13 -0400

    Fixed copy error.

--- MassCopy/MassCopyTools.vb
+++ MassCopy/MassCopyTools.vb
@@ -1,6 +1,6 @@
 ﻿Public Module MassCopyTools
     Public Function GetDirNameFromPath(FilePath As String) As String
         Dim split As String() = FilePath.Split("\")
-        Return split(split.Length - 2)
+        Return split(split.Length - 1)
     End Function
 End Module
